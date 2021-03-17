f5cc8f72588e29663c19c9fb031749d47d574102
dalia a01635883 <a01635883@itesm.mx>
2021-03-17 01:27:49 +0000

    practica2

--- xv6-public/init.c
+++ xv6-public/init.c
@@ -20,13 +20,15 @@ main(void)
   dup(0);  // stderr
 
   for(;;){
-    printf(1, "init: starting sh\n");
+    printf(1, "las focas son perros sirena sh\n");
     pid = fork();
     if(pid < 0){
       printf(1, "init: fork failed\n");
       exit();
     }
     if(pid == 0){
+		//1) con ls se genera un loop infinito de directorios
+		//con no existe archivo sale un loop infinito de exec sh failed
       exec("sh", argv);
       printf(1, "init: exec sh failed\n");
       exit();
