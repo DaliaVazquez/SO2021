b9eb6fbd81e7a854babf5686641c77b9f764de5c
Dalia <a01635883@itesm.mx>
2021-04-07 00:48:43 +0000

    prectica 6

--- IPC/cuentalineas.c
+++ IPC/cuentalineas.c
@@ -2,14 +2,13 @@
 #include <ctype.h>
 int main(){
     char minC;
-    int num=1;
+    int num=0;
     while(read(0,&minC,1)){
         if(minC=='\n'){
             num ++;
         }
         
     }
-    
     printf("num de lineas: %d\n", num);
     return 0;
 }
