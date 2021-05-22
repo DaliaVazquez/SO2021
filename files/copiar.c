6398a02fc60537151b3295a9203a92e4a6be0cdc
Dalia <a01635883@itesm.mx>
2021-05-22 05:00:44 +0000

    Práctica 11

--- files/copiar.c
+++ files/copiar.c
@@ -6,33 +6,36 @@ int main(int argnum, char **argv){
     char *original = argv[1];
     char *destino = argv[2];
     char buffer;
-    int size;
+    char *linea;
+    int size=0;
     FILE *foriginal, *fdestino;
     foriginal = fopen(original,"rb");
     fdestino = fopen(destino,"wb");
-    while(fread(&buffer,sizeof(char),1,foriginal) ){
-        
-        char *linea="";
+    int bol=fread(&buffer,sizeof(char),1,foriginal); ;
+    while(bol){
+        size += 1;
         if(buffer == '\n'){
-            
-            if(strstr(linea,"hola") == ""){
-                fwrite(linea,sizeof(char),size,foriginal);
+            fseek(foriginal, -size, SEEK_CUR);
+            linea = (char *) calloc(size, sizeof(char));
+            fread(linea,sizeof(char),size,foriginal);
+            if(strstr(linea,"hola") == NULL){
+                fwrite(linea,sizeof(char),size,fdestino);
             }
             size = 0;
-            
-        } else {
-            //size += 1;
-            /*fwrite(&buffer,sizeof(char),1,fdestino);
-            size += 1;
-            fseek(foriginal, -size, SEEK_CUR);
-            linea = (char *) calloc(size, sizeof(char)); // linea[size];
-            fread(linea,sizeof(char),size,foriginal);*/
         }
-        //fwrite("&buffer,sizeof(char),1,fdestino");
+        bol=fread(&buffer,sizeof(char),1,foriginal); 
+        if(!bol){
+            fseek(foriginal, -size, SEEK_CUR);
+            linea = (char *) calloc(size, sizeof(char)); 
+            fread(linea,sizeof(char),size,foriginal);
+            if(strstr(linea,"hola") == NULL){
+                fwrite(linea,sizeof(char),size,fdestino);
+            }
+        }
     }
+    
     fclose(foriginal);
-    fclose(fdestino);/**/
+    fclose(fdestino);
 
     return 0;
 }
-
