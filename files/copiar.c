574e1c821b40952f67063a69938722fd865d10b0
Dalia <a01635883@itesm.mx>
2021-05-22 03:27:47 +0000

    copia1

--- files/copiar.c
+++ files/copiar.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main(int argnum, char **argv){
     char *original = argv[1];
@@ -9,23 +11,27 @@ int main(int argnum, char **argv){
     foriginal = fopen(original,"rb");
     fdestino = fopen(destino,"wb");
     while(fread(&buffer,sizeof(char),1,foriginal) ){
+        
+        char *linea="";
         if(buffer == '\n'){
-            char *linea;
+            
+            if(strstr(linea,"hola") == ""){
+                fwrite(linea,sizeof(char),size,foriginal);
+            }
+            size = 0;
+            
+        } else {
+            //size += 1;
+            /*fwrite(&buffer,sizeof(char),1,fdestino);
             size += 1;
             fseek(foriginal, -size, SEEK_CUR);
             linea = (char *) calloc(size, sizeof(char)); // linea[size];
-            fread(linea,sizeof(char),size,forinal)
-            if(strtr(linea,"hola") == NULL){
-                fwrite()
-            }
-            size = 0;
-        } else {
-            size += 1;
+            fread(linea,sizeof(char),size,foriginal);*/
         }
-        fwrite(&buffer,sizeof(char),1,fdestino);
+        //fwrite("&buffer,sizeof(char),1,fdestino");
     }
     fclose(foriginal);
-    fclose(fdestino);
+    fclose(fdestino);/**/
 
     return 0;
 }
